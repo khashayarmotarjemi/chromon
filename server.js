5d6b9c7490bb458beb6330d074503521695d44b0
khashayar <khashayar.k.m@gmail.com>
2020-07-15 17:31:36 +0430

    bux fix: pages were being retrieved only once

--- server.js
+++ server.js
@@ -7,52 +7,32 @@ let interval = 3000;
     // set some options (set headless to false so we can see 
     // this automated browsing experience)
     let launchOptions = { headless: false, 
-                          executablePath: '/usr/bin/chromium', // because we are using puppeteer-core so we must define this option
+                          executablePath: '/usr/bin/chromium',
                           args: ['--no-sandbox'],
                         userDataDir: '~/.config/chromium' };
 
     const browser = await puppeteer.launch(launchOptions);
 
-    const pages = await browser.pages();
-
-
     setInterval(async () => {
+      const pages = await browser.pages();
+
       pages.forEach( async (page) => {
         let title = await page.title();
-        if(title.startsWith("asdf")) {
+        if(title.startsWith("www.goo")) {
 
             // mute
             console.log("mute");
 
-
             let unmuteInterval = setInterval(async () => {
                 let title = await page.title();
-                if(!title.startsWith("asdf")) {
+                if(!title.startsWith("www.goo")) {
                     console.log('unmute');
                     clearInterval(unmuteInterval);
                 }
             }, 1000);
         }
-    })
-
-    
+    })  
     }, interval);
-
-      
-   
-    // const page = await browser.newPage();
-
-    // // set viewport and user agent (just in case for nice viewing)
-    // await page.setViewport({width: 1366, height: 768});
-    // await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
-
-    // // go to the target web
-    // await page.goto('https://google.com');
-
-    // console.log(await browser.version());
-
-    // close the browser
-    // await browser.close();
 })();
 
     
